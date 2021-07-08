3618eb0d2c3bdc4d1203dfc6437a1a98072a3382
nadundanushka696969 <naudunstudy2020@gmail.com>
2021-07-08 10:17:32 +0530

    aaaa

--- PlayerSelection.java
+++ PlayerSelection.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class PlayerSelection{
 	ArrayList<String> players=new ArrayList<String>();
+	static Scanner input=new Scanner(System.in);
 	public static void main(String[] args){
-		Scanner input=new Scanner(System.in);
 		String lowwercaseInput=input.nextLine().toLowerCase();
 		//String lowwercaseInput=input.toLowerCase();
 		System.out.println("press A to add player");
@@ -13,17 +13,44 @@ public class PlayerSelection{
 		player(lowwercaseInput);
 	} 
 
-	public static String player(String input){
+	public static void player(String input){
+		System.out.println(input);
+		System.out.println("Enter player ");
+		System.out.println("Name	:");
+		String name=input.nextLine();
+        System.out.println("Age 	:");
+        String name=input.nextInt();
+        System.out.println("Type 	:");
+        int name=input.nextLine();
+        System.out.println("Statistics :");
+        String name=input.nextLine();
+
+
 		switch(input){
 			case "a":
-				System.out.println("press a to add player");
+				getPlayerType();
+				Player ply1=new player(name, age, type, statistics);
+				//System.out.println("press a to add player");
+				break;
 			case "b":
-				System.out.println("press b to add player");
+				getBestPerformence();
+				//System.out.println("press b to add player");
+				break;
 			case "c":
-				System.out.println("press c to add player");
+				getBestPerformence();
+				//System.out.println("press c to add player");
+				break;
+			case "v":
+				playerDetails();
+				//System.out.println("press c to add player");
+				break;
 			default:
 				System.out.println("Try again...");
 
 		}
+	}
+
+	public String best2player(){
+			
 	}
 }
